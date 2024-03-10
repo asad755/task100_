9b23d9a5430aa3cc4c9991583b36d38eeac9d2ab
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-10 16:21:53 +0500

    asharib

---
+++ main10.js
@@ -0,0 +1,4 @@
+// Asadiftekhar, 2024-03-05
+// This program prints a personal message.
+var myName = "Asadiftekhar";
+console.log("Hello ".concat(myName, ", would you like to learn some TypeScript today?"));
---
+++ main10.ts
@@ -0,0 +1,4 @@
+// Asadiftekhar, 2024-03-05
+// This program prints a personal message.
+let myName: string = "Asadiftekhar";
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`);
