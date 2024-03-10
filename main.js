d8326d38084e4998aaba5764dd47a51620c1d11d
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-10 16:15:21 +0500

    asad

---
+++ main.js
@@ -0,0 +1,12 @@
+//excersize 1 Q45 assinment 
+var personName = "ERIC";
+console.log("hellow ".concat(personName, " would you like to learn some Python today?"));
+//excersize 2 Q45 assinment 
+//excersize 3 Q45 assinment 
+//excersize 4 Q45 assinment 
+//excersize 5 Q45 assinment 
+//excersize 6 Q45 assinment 
+//excersize 7 Q45 assinment 
+//excersize 8 Q45 assinment 
+//excersize 9 Q45 assinment 
+//excersize 10 Q45 assinment 
---
+++ main1.js
@@ -0,0 +1,2 @@
+var myName = "Asadiftekhar"; // This saves the name
+console.log("Hello ".concat(myName, ", would you like to learn some TypeScript today?")); // This shows the message
