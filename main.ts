aa994294398250920d05850397355ca808934354
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-15 09:51:19 +0500

    new8

--- main.ts
+++ main.ts
@@ -1,17 +1,2 @@
-//excersize 1 Q45 assinment 
-var personName = "ERIC";
-console.log("hellow ".concat(personName, " would you like to learn some Python today?"));
-
-
-
-
-
-//excersize 2 Q45 assinment 
-//excersize 3 Q45 assinment 
-//excersize 4 Q45 assinment 
-//excersize 5 Q45 assinment 
-//excersize 6 Q45 assinment 
-//excersize 7 Q45 assinment 
-//excersize 8 Q45 assinment 
-//excersize 9 Q45 assinment 
-//excersize 10 Q45 assinment 
+let favoriteNumber: number = 8; // This saves your favorite number
+console.log(`My favorite number is ${favoriteNumber}.`); // This shows the message with your favorite number
