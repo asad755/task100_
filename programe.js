3c476ec147025067a25faff912b14223a1f17cdd
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-15 16:13:26 +0500

    with baber

---
+++ programe.js
@@ -0,0 +1,5 @@
+//excersize 10
+//my name is Asadullah
+//Dated: 19/02/2024
+//this programme will multiplication
+console.log(5 * 2);
---
+++ programe.ts
@@ -0,0 +1,6 @@
+//excersize 10
+//my name is Asadullah
+//Dated: 19/02/2024
+//this programme will multiplication
+
+console.log(5 * 2);
